8c85d5d119dcb59fa1a93f5dc85b3d95231eae51
Suren1997 <surendar16cool@gmail.com>
2026-06-18 08:54:54 +0530

    Completed Eduction Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import Education from "./components/Education";
 
 const App = () => {
   useEffect(() => {
@@ -16,6 +17,7 @@ const App = () => {
     <div className="bg-[#111827] min-h-screen">
       <Header />
       <HeroSection />
+      <Education />
     </div>
   );
 };
